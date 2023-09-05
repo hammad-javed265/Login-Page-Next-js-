13ba5f0fdbd76ec26bd4b2df7f6c78f24d41d8a6
Hammad <hammadjaved265@gmail.com>
2023-09-05 16:29:49 +0500

    newcommit

--- src/app/components/Dropdown.js
+++ src/app/components/Dropdown.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 // import Link from 'next/link';
 
-const Dropdown = ({ title, children, img, adjust }) => {
+const Dropdown = ({ title, children, imgSrc,imgClassName, adjust }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeDropdown, setActiveDropdown] = useState(null);
 
@@ -36,7 +36,7 @@ const Dropdown = ({ title, children, img, adjust }) => {
         onClick={toggleDropdown}
         className={"px-2 py-1 rounded flex items-center"}
       >
-        <span className="mr-1">{title}{img}</span>
+         <span className="mr-1">{title} <img src={imgSrc} className={imgClassName} /></span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
