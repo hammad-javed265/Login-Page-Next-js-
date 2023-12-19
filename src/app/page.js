69d405413b1c51448fd8098ab6c61f21e58daddb
hammad-javed265 <hammadjaved265@gmail.com>
2023-12-19 16:16:23 +0500

    noo

--- src/app/page.js
+++ src/app/page.js
@@ -16,10 +16,10 @@ function page() {
       {/* navbar starts */}
       <NavList></NavList>
       {/* navbar ends */}
-      <div className="flex flex-wrap space-x-6 space-y-6 sm:h-full md:h-[130vh] lg:h-[100vh] xl:h-[95vh]">
+      <div className="flex flex-wrap space-x-6 space-y-6 sm:h-full md:h-[130vh] lg:h-[100vh] xl:h-[85vh]">
         <div className="flex flex-wrap flex-col w-full h-full">
-          <Div id={"div1"} title={"Live Energy Consumption"} length={"lg:w-[26%] xl:w-[26%] mr-6"} heig = "md:h-[404px] lg:h-[404px] xl:h-[380px]"><GaugeChart></GaugeChart></Div>
-          <Div id={"div7"} title={"Radius Pie Chart"} length={"lg:w-[26%] xl:w-[26%] mr-6"} heig = "md:h-[404px] lg:h-[404px] xl:h-[380px]"><Radiuspie></Radiuspie></Div>
+          <Div id={"div1"} title={"Live Energy Consumption"} length={"lg:w-[26%] xl:w-[26%] mr-6"} heig = "h-[380px] max-lg:h-[404px]"><GaugeChart></GaugeChart></Div>
+          <Div id={"div7"} title={"Radius Pie Chart"} length={"lg:w-[26%] xl:w-[26%] mr-6"} heig = "h-[380px] max-lg:h-[404px]"><Radiuspie></Radiuspie></Div>
  
           <div className="flex flex-wrap w-full xl:h-[120px] h-[300px] sm:h-[300px] md:h-[170px] lg:h-[170px] sm:w-full md:w-[44%] lg:w-[44%] xl:w-[44%] mt-6">
             <Consump id={"today"} title={"Today"} xltop={"xl:mt-0"}>337 kWh</Consump>
@@ -30,8 +30,8 @@ function page() {
           <Div id={"div4"} title={"Period Over Period Chart"} length={"w-full sm:w-full md:w-[44%] lg:w-[44%] xl:w-[44%] ml-[0px]"} heig = "h-[307px]"><VulnChart></VulnChart></Div>
           <Div id={"div2"} title={"Live Power Factor"} length={"w-full sm:w-full md:w-[44%] lg:w-[44%] xl:w-[44%] ml-[0px]"} heig = "h-[307px]"><Realtime></Realtime></Div>
      
-          <Div id={"div5"} title={"Pie Chart"} length={"lg:w-[26%] xl:w-[26%]  mt-6"} heig = "md:h-[404px] lg:h-[404px] xl:h-[380px]"><PieChart></PieChart></Div>
-          <Div id={"div6"} title={"Donut Chart"} length={"lg:w-[26%] xl:w-[26%] "} heig = "md:h-[404px] lg:h-[404px] xl:h-[380px]"><Donut></Donut></Div>
+          <Div id={"div5"} title={"Pie Chart"} length={"lg:w-[26%] xl:w-[26%]  mt-6"} heig = "h-[380px] max-lg:h-[404px]"><PieChart></PieChart></Div>
+          <Div id={"div6"} title={"Donut Chart"} length={"lg:w-[26%] xl:w-[26%] "} heig = "h-[380px] max-lg:h-[404px]"><Donut></Donut></Div>
           
         </div>
 
